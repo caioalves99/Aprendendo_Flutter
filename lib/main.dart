c16cbc79d4d0cf2acf7cf791085cb8e950c186d3
Caio Alves <c.ads9960@gmail.com>
2025-08-07 10:04:07 -0400

    UPDATE MAIN

--- lib/main.dart
+++ lib/main.dart
@@ -7,27 +7,27 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Este widget é a raiz da sua aplicação.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Aprendendo Flutter',
+      title: 'App Flutter',
       theme: ThemeData(
-        // This is the theme of your application.
+       // Este é o tema da sua aplicação.
         //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
+        // EXPERIMENTE: Tente executar sua aplicação com "flutter run". Você verá
+        // que a aplicação tem uma barra superior roxa. Então, sem fechar o app,
+        // tente mudar o seedColor no colorScheme abaixo para Colors.green
+        // e então faça um "hot reload" (salve suas alterações ou pressione o botão
+        // de "hot reload" em um IDE que suporte Flutter, ou pressione "r" se você
+        // iniciou o app pelo terminal).
         //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
+        // Note que o contador não voltou para zero; o estado da aplicação
+        // não é perdido durante o reload. Para resetar o estado, use o hot
+        // restart.
         //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
+        // Isso funciona para código também, não só para valores: A maioria das alterações
+        // no código podem ser testadas apenas com um hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
       ),
       home: const MyHomePage(title: 'Aprendendo Flutter'),
@@ -38,14 +38,14 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+ // Este widget é a página inicial da sua aplicação. Ele é stateful, ou seja,
+  // possui um objeto State (definido abaixo) que contém campos que afetam
+  // como ele é exibido.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  // Esta classe é a configuração para o estado. Ela armazena os valores (neste
+  // caso o título) fornecidos pelo pai (neste caso o widget App) e
+  // usados pelo método build do State. Campos em uma subclasse de Widget são
+  // sempre marcados como "final".
 
   final String title;
 
@@ -58,50 +58,49 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+       // Esta chamada ao setState informa ao framework Flutter que algo foi
+      // alterado neste State, o que faz com que o método build abaixo seja
+      // executado novamente para que a tela reflita os valores atualizados. Se
+      // alterássemos _counter sem chamar setState(), o método build não seria
+      // chamado novamente e nada aconteceria visualmente.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // Este método é executado novamente toda vez que setState é chamado, por exemplo,
+    // como feito pelo método _incrementCounter acima.
     //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // O framework Flutter foi otimizado para tornar a execução repetida do build rápida,
+    // assim você pode simplesmente reconstruir qualquer coisa que precise ser atualizada
+    // ao invés de alterar individualmente instâncias de widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+       // EXPERIMENTE: Tente mudar a cor aqui para uma cor específica (talvez
+        // Colors.amber?) e faça um hot reload para ver a AppBar mudar de cor
+        // enquanto as outras cores permanecem as mesmas.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+       // Aqui pegamos o valor do objeto MyHomePage que foi criado pelo
+        // método App.build, e usamos para definir o título da appbar.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+         // Center é um widget de layout. Ele recebe um único filho e o posiciona
+        // no centro do pai.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
+          // Column também é um widget de layout. Ele recebe uma lista de filhos e
+          // os organiza verticalmente. Por padrão, ajusta seu tamanho para caber
+          // nos filhos horizontalmente e tenta ser tão alto quanto o pai.
           //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
+          // Column possui várias propriedades para controlar como se ajusta e
+          // como posiciona seus filhos. Aqui usamos mainAxisAlignment para
+          // centralizar os filhos verticalmente; o eixo principal aqui é o eixo
+          // vertical porque Columns são verticais (o eixo cruzado seria
           // horizontal).
           //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+          // EXPERIMENTE: Ative o "debug painting" (escolha a ação "Toggle Debug Paint"
+          // na IDE, ou pressione "p" no console), para ver o wireframe de cada widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text('Você pressionou o botão esta quantidade de vezes:'),
@@ -116,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), // Esta vírgula final deixa a formatação automática mais agradável para métodos build.
     );
   }
 }
