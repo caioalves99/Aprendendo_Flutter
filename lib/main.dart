3a8c4add16cfccecb4b13794bc254977d9add067
Caio Alves <c.ads9960@gmail.com>
2025-08-18 08:38:47 -0400

    Aprendendo sobre MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 50, color: Colors.red),
-        ),
-      ),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.indigo,),
+      home: Container(
+        child: Center(child: Text('Projeto teste')),
+      )
     );
   }
 }
