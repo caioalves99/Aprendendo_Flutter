0b752f6b90bd46e0efab134fa5c5942632e2c123
Caio Alves <c.ads9960@gmail.com>
2025-08-18 07:16:31 -0400

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
 
-main(){
-  runApp(MyApp(title: "Projeto Flutter",));
+main() {
+  runApp(MyApp(title: 'Projeto Flutter'));
 }
 
 class MyApp extends StatelessWidget {
-  
   final String title;
 
   const MyApp({super.key, required this.title});
-  
-  
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,8 +17,8 @@ class MyApp extends StatelessWidget {
           title,
           textDirection: TextDirection.ltr,
           style: TextStyle(fontSize: 50, color: Colors.red),
-        )
-      )
+        ),
+      ),
     );
   }
 }
