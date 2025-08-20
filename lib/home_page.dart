f81647bcbe32713afa25be0396ebcbb70c532535
Caio Alves <c.ads9960@gmail.com>
2025-08-20 16:24:25 -0400

    Aprendendo sobre Gerenciar outro Estado

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:aprendendo_flutter/app_controller.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,27 +10,23 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   int counter = 0;
+  bool isDartTheme = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Página Inicial'),
-         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-           
-      body: Container(
-        height: 200,
-        width: 200,
-        color: Colors.blue,
-          
-        child: Center(
-          child: Container(
-            height: 100,
-            width: 100,
-            color: Colors.tealAccent,
-          )
-      )
+
+      body: Center(
+        child: Switch(
+          value: AppController.instance.isDartTheme,
+          onChanged: (value) {
+            AppController.instance.changeTheme();
+          },
+        ),
       ),
 
       floatingActionButton: FloatingActionButton(
@@ -40,7 +37,6 @@ class HomePageState extends State<HomePage> {
           });
         },
       ),
-
     );
   }
 }
