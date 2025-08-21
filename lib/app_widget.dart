750a0dd5d3060544575907d2c7b4f5241a88f530
Caio Alves <c.ads9960@gmail.com>
2025-08-21 07:34:49 -0400

    Update

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -3,6 +3,8 @@ import 'package:aprendendo_flutter/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
@@ -10,7 +12,7 @@ class AppWidget extends StatelessWidget {
       builder: (context, child) {
         return MaterialApp(
           theme: ThemeData(
-            primarySwatch: Colors.grey,
+            primarySwatch: Colors.cyan,
             brightness: AppController.instance.isDartTheme 
               ? Brightness.dark 
               : Brightness.light,
