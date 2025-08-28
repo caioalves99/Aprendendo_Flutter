35cbf2d81ed875c1a15f46904f15e285aaccc652
Caio Alves <c.ads9960@gmail.com>
2025-08-28 12:38:12 -0400

    stack (Empilhamento)

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,68 +12,92 @@ class _LoginPageState extends State<LoginPage> {
   String email = "";
   String senha = "";
 
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                Container(
-                  width: 300,
-                  height: 300,
-                  child: Image.asset('assets/images/logo.png'),
+  Widget _body() {
+    return Column(
+        children: [
+          SingleChildScrollView(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+          
+                    SizedBox(
+                      width: 300,
+                      height: 300,
+                      child: Image.asset('assets/images/logo.png'),
+                    ),
+          
+                    SizedBox(height: 20),
+          
+                    TextField(
+                      onChanged: (text) {
+                        email = text;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+          
+                    SizedBox(height: 20),
+          
+                    TextField(
+                      onChanged: (text) {
+                        senha = text;
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Senha",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+          
+                    SizedBox(height: 20),
+          
+                    ElevatedButton(
+                      onPressed: () {
+                        if (email == 'caio@sub.com' && senha == '123') {
+                          Navigator.of(context).pushReplacementNamed('/home');
+                        } else {
+                          print("Login inválido");
+                        }
+                      },
+                      child: Text("Entrar"),
+                    ),
+                  ],
                 ),
-
-                SizedBox(height: 20),
-
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: "Email",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-
-                SizedBox(height: 20),
-
-                TextField(
-                  onChanged: (text) {
-                    senha = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: "Senha",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-
-                SizedBox(height: 20),
-
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'caio@sub.com' && senha == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print("Login inválido");
-                    }
-                  },
-                  child: Text("Entrar"),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      ),
+        ],
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            //width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset('assets/images/background.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          Container(
+            color: Colors.black.withOpacity(0.4),
+          ),
+          
+          _body(),
+        ],
+      )
     );
   }
 }
