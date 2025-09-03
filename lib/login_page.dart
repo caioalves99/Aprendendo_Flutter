5713992baf4dbf72ed3b63a8f24e736f22659481
Caio Alves <c.ads9960@gmail.com>
2025-09-02 23:44:30 -0400

    Update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,86 +12,69 @@ class _LoginPageState extends State<LoginPage> {
   String email = "";
   String senha = "";
 
+  //@override
   Widget _body() {
-    return Column(
-        children: [
-          SingleChildScrollView(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-          
-                    SizedBox(
-                      width: 300,
-                      height: 300,
-                      child: Image.asset('assets/images/logo.png'),
-                    ),
-          
-                    SizedBox(height: 20),
-          
-                    Card(
-                      color: Colors.white.withOpacity(0.7),
-                      child:
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Column(
-                            children: [
-                              TextField(
-                                onChanged: (text) {
-                                  email = text;
-                                },
-                                keyboardType: TextInputType.emailAddress,
-                                decoration: InputDecoration(
-                                  labelText: "Email",
-                                  border: OutlineInputBorder(),
-                                ),
-                              ),
-                                    
-                              SizedBox(height: 20),
-                                    
-                              TextField(
-                                onChanged: (text) {
-                                  senha = text;
-                                },
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  labelText: "Senha",
-                                  border: OutlineInputBorder(),
-                                ),
-                              ),
-
-                              SizedBox(height: 20),
-
-                              ElevatedButton(
-                                onPressed: () {
-                                  if (email == 'caio@sub.com' && senha == '123') {
-                                    Navigator.of(context).pushReplacementNamed('/home');
-                                  } else {
-                                    print("Login inválido");
-                                  }
-                                },
-                                child: Text("Entrar"),
-                              ),
-                              
-                            ],
-                          ),
+    return Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: 300,
+                height: 300,
+                child: Image.asset('assets/images/logo.png'),
+              ),
+              SizedBox(height: 20),
+              Card(
+                color: Colors.white.withOpacity(0.9),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(),
                         ),
-                    ),
-          
-                    SizedBox(height: 20),
-          
-                    
-                  ],
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        onChanged: (text) {
+                          senha = text;
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Senha",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (email == 'caio@sub.com' && senha == '123') {
+                            Navigator.of(context).pushReplacementNamed('/home');
+                          } else {
+                            print("Login inválido");
+                          }
+                        },
+                        child: Text("Entrar"),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
+              SizedBox(height: 20),
+            ],
           ),
-        ],
-      );
+        ),
+      ), 
+    );
   }
 
   @override
@@ -100,20 +83,19 @@ class _LoginPageState extends State<LoginPage> {
       body: Stack(
         children: [
           SizedBox(
-            //width: double.infinity,
+            width: double.infinity,
             height: MediaQuery.of(context).size.height,
-            child: Image.asset('assets/images/background.jpg',
+            child: Image.asset(
+              'assets/images/background.jpg',
               fit: BoxFit.cover,
             ),
           ),
-
           Container(
             color: Colors.black.withOpacity(0.4),
           ),
-          
           _body(),
         ],
-      )
+      ),
     );
   }
 }
